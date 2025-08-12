9e12b366f15f1134eb75a100db3bc158b257bf46
Peter <schmidt.multimedia@gmail.com>
2025-08-12 10:51:54 +0200

    04_tables / pw als Text

--- 02_tables/04_tables.sql
+++ 02_tables/04_tables.sql
@@ -18,21 +18,20 @@ CREATE TABLE IF NOT EXISTS kunde
     k_id INT AUTO_INCREMENT PRIMARY KEY,
     k_name VARCHAR(45) NOT NULL DEFAULT 'TBA',
     k_alter TINYINT,
-    k_mail VARCHAR(100) UNIQUE
+    k_mail VARCHAR(100) UNIQUE,
+    k_pw VARCHAR(100)
 );
 
 -- Struktur anzeigen
 DESCRIBE kunde;
 
 -- Datensätze/Zeilen
-INSERT INTO kunde(k_id,k_name,k_alter,k_mail) VALUES(DEFAULT,"Meier", 28, "meier@web.de");
-INSERT INTO kunde(k_id,k_name,k_alter,k_mail) VALUES(DEFAULT,"Hussein", 32, "hussein@web.de");
-INSERT INTO kunde(k_id,k_name,k_alter,k_mail) VALUES(DEFAULT,"Li", 26,"li@web.de");
-INSERT INTO kunde(k_id,k_name,k_alter,k_mail) VALUES(DEFAULT,"Li", 40,"li2@web.de");
-INSERT INTO kunde(k_id,k_name,k_alter,k_mail) VALUES(DEFAULT,"Li", 62,"li_shanghai@web.de");
+INSERT INTO kunde(k_id,k_name,k_alter,k_mail,k_pw) VALUES(DEFAULT,"Meier", 28, "meier@web.de",'maier1234');
+INSERT INTO kunde(k_id,k_name,k_alter,k_mail,k_pw) VALUES(DEFAULT,"Hussein", 32, "hussein@web.de",'hus#!12');
+INSERT INTO kunde(k_id,k_name,k_alter,k_mail,k_pw) VALUES(DEFAULT,"Li", 26,"li@web.de",'li1234');
+INSERT INTO kunde(k_id,k_name,k_alter,k_mail,k_pw) VALUES(DEFAULT,"Li", 40,"li2@web.de", '#!aBcD');
+INSERT INTO kunde(k_id,k_name,k_alter,k_mail,k_pw) VALUES(DEFAULT,"Li", 62,"li_shanghai@web.de",'wau12');
 
-INSERT INTO kunde(k_id,k_name,k_alter,k_mail) VALUES(DEFAULT,NULL,NULL,NULL);
-INSERT INTO kunde() VALUES();
 
 -- Tabellendaten (Inhalte) anzeigen
 SELECT * FROM kunde;
