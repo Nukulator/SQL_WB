d2cbfb35ae14ee29e303d30193bf9f0b2d9cb59c
Peter <schmidt.multimedia@gmail.com>
2025-08-12 12:50:32 +0200

    selects update

---
+++ 03_select/02_select.sql
@@ -0,0 +1,27 @@
+\! cls
+
+--  02 SELECT
+
+-- basic
+-- SELECT 
+--     *
+-- FROM boo.contacts
+-- ;
+
+
+-- SELECT 
+--     id,
+--     email
+-- FROM boo.contacts
+-- ;
+
+SELECT 
+    id,
+    email,
+    city
+FROM boo.contacts
+WHERE city = 'Berlin'
+;
+
+
+
